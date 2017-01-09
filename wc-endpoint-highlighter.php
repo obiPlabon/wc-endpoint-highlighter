c5bd2a7ed50c6816574f894719505e6ea9a7a294
Plabon <obi.plabon@gmail.com>
2017-01-09 12:35:57 +0600

    Updated

--- wc-endpoint-highlighter.php
+++ wc-endpoint-highlighter.php
@@ -1,14 +1,39 @@
 <?php
-/**
- * Plugin Name: WC Endpoint Highlighter
- * Plugin URI: http://obiPlabon.im/
- * Author: Obi Plabon
- * Author URI: http://obiPlabon.im/
- * 
- */
+/*
+Plugin Name: WC Endpoint Highlighter
+Plugin URI: 
+Description: "WooCommerce Endpoint" label is added to all WooCommerce endpoint menu items.
+Version: 0.0.7
+Author: Obi Plabon
+Author URI: http://obiPlabon.im/
+License: GPLv2 or later
+Text Domain: wc-endpoint-highlighter
+*/
 
-if ( !defined( 'ABSPATH' ) ) {
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2015 Automattic, Inc.
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
 	die();
+}
+
+if ( ! function_exists( 'WC' ) ) {
+	return;
 }
 
 class Wc_Endpoint_Highlighter {
